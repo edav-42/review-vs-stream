a86f5940df69f0fcda200f819b26f079268c364d
zcysxy <zcysxy.wyzdz@gmail.com>
2022-10-31 17:29:30 -0400

    remove spotify token

--- data/spotify.R
+++ data/spotify.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(spotifyr)
-Sys.setenv(SPOTIFY_CLIENT_ID = '8cccb4c2ec73415782df7737df5ce6d5')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = '03cd502aae094f688594d28ba2ba29fa')
+token <- read.csv('spotify_token')
+Sys.setenv(SPOTIFY_CLIENT_ID = token[1,1])
+Sys.setenv(SPOTIFY_CLIENT_SECRET = token[1,2])
 
 access_token <- get_spotify_access_token()
 res <- search_spotify('future nostalgia, dua lipa', type="album")[1,5] %>% 
