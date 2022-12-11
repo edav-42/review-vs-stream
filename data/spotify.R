97b60ccd5fcbf54bd41c5194241eaa4775eb115b
Yihan Chen <1187731462@qq.com>
2022-12-10 20:04:01 -0500

    add spotify.csv

--- data/spotify.R
+++ data/spotify.R
@@ -5,12 +5,30 @@ Sys.setenv(SPOTIFY_CLIENT_ID = token[1,1])
 Sys.setenv(SPOTIFY_CLIENT_SECRET = token[1,2])
 
 access_token <- get_spotify_access_token()
-res <- search_spotify('future nostalgia, dua lipa', type="album")[1,5] %>% 
-  get_album_tracks()
-res <- get_tracks(res$id) %>%
-  group_by(album.name) %>% 
-  summarise(
-    avg_pop = mean(popularity),
-    max_pop = max(popularity),
-    sum_pop = sum(popularity)
-  )
+
+# given album name and singer name, get stream popularity
+get_spotify_data <- function(album, singer){
+  res <- search_spotify(paste(album, singer, sep = ", "), type="album")[1,5] %>% 
+    get_album_tracks()
+  res <- get_tracks(res$id) %>%
+    group_by(album.name) %>% 
+    summarise(
+      avg_pop = mean(popularity),
+      max_pop = max(popularity),
+      sum_pop = sum(popularity)
+    )
+  return(res)
+}
+
+# for music in metacritic_data.csv get spotify popularity
+df <- read.csv("data/metacritic_data.csv")
+spotify_data = data.frame(matrix(ncol = 4, nrow = 0))
+for(i in 2158:nrow(df)){
+  row = get_spotify_data(df[i, "Album_Name"], df[i, "Singer"])
+  spotify_data <- rbind(spotify_data, row)
+  print(i)
+}
+
+
+# export to csv
+write.csv(spotify_data, "data/spotify.csv", row.names=FALSE)
